cdd193350bd1980fe64bb2585039f723651c4ebf
Inga <inga.lingiene12@gmail.com>
2022-06-16 16:51:03 +0300

    b

--- my-react/src/Components/Create.jsx
+++ my-react/src/Components/Create.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 function Create() {
+    //kiek laukeliu returne tiek turim tureti useState(react xxx)
+    const [ vardas, setVardas] = useState('');
+    const [ tipas, setTipas] = useState("1");
+    const [ kaina, setKaina] = useState('');
+    const [ trukme, setTrukme] = useState('');
+    const [ nuotrauka, setNuotrauka] = useState('');
 
     return(
         <div className="stulpeliu-vaikas2">
@@ -9,11 +17,11 @@ function Create() {
                 <div className="formos-vidus">
                     <div className="forma">
                         <label>Manikiuro meistre:</label>
-                        <input type="text" className="form-control" placeholder="Manikiuro meistre" />
+                        <input type="text" className="form-control" placeholder="Manikiuro meistre" value={vardas} />
                     </div>
                     <div className="forma">
                         <label>Manikiūro rūšys:</label>
-                        <select className="form-control" >
+                        <select className="form-control" value={tipas} >
                             <option  value="1">Klasikinis manikiūras</option>
                             <option  value="2">Prancūziškas manikiūras</option>
                             <option  value="3">Kombinuotas manikiūras</option>
@@ -21,15 +29,15 @@ function Create() {
                     </div>
                     <div className="forma">
                         <label>Kaina:</label>
-                        <input type="text" className="form-control" placeholder="Kaina eurais" />
+                        <input type="text" className="form-control" placeholder="Kaina eurais" value={kaina} />
                     </div>
                     <div className="forma">
                         <label>Trukme:</label>
-                        <input type="text" className="form-control"placeholder="Trukme" />
+                        <input type="text" className="form-control"placeholder="Trukme" value={trukme} />
                     </div>
                     <div className="forma">
                         <label>Nuotrauka:</label>
-                        <input  type="file" className="forma-foto"  />
+                        <input  type="file" className="forma-foto" value={nuotrauka} />
                     </div>
                     <div className="forma-buttonss">
                         <button type="button" className="forma-buttons">Irasyti</button>
